687f1d40d87d54fc2a8e9c86cbe576c5689a94b5
Braigtom <Braigtom@LAPTOP-CQC9OH22>
2020-10-08 16:20:30 -0500

    socket

--- src/sockets/cliente.java
+++ src/sockets/cliente.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sockets;
 
 import java.io.IOException;
 import java.io.OutputStream;  import java.net.InetAddress;  import java.net.Socket;
+import java.util.Scanner;
 
 public class cliente {
 
@@ -14,15 +11,25 @@ public class cliente {
     Socket        client;
     OutputStream  os;
     InetAddress   ia;
+    Scanner entrada=new Scanner(System.in);
 
     try {
-      ia = InetAddress.getByName("10.42.0.1");//get local host address
-      
+      ia = InetAddress.getByName("localhost");//get local host address
+     
       client = new Socket(ia, 9999); //create socket (*@\clientBox{1+2)}@*)
-      
-      os = client.getOutputStream(); //get stream to write to
-      os.write(1);  //write one byte of value 1 (*@\clientBox{3)}@*)
-      
+     
+      os = client.getOutputStream();
+     
+      int number=0;
+      while (number>=20 || number<=0)
+      { System.out.println("Ingrese un numero");
+      number = entrada.nextInt();
+     
+      os = client.getOutputStream(); //conseguir que la corriente escriba
+     
+      }
+      os.write(number);  //write one byte of value 1 (*@\clientBox{3)}@*)
+     
       client.close(); //close (*@\clientBox{4)}@*)
     } catch (IOException t) {
     }
--- src/sockets/servidor.java
+++ src/sockets/servidor.java
@@ -1,37 +1,40 @@
 package sockets;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Braigtom
- */
+import java.io.IOException;
 import java.io.InputStream;   import java.net.ServerSocket;   import java.net.Socket;
 
 public class servidor {
-  public static final void main(final String[] args) {
+  public static final void main(final String[] args) throws IOException {
     ServerSocket server;           InputStream  is;
     Socket       client;          
 
     try {
       server = new ServerSocket(9999);//(*@\serverBox{1 + 2)}@*)
-      
+     
       for (int j = 5; (--j) >= 0;) {  //process only 5 clients, so I can show (*@\serverBox{5)}@*) below
         client = server.accept(); //wait for incoming connection (*@\serverBox{3)}@*)
         System.out.println("New connection from " + client.getRemoteSocketAddress());
-        
-        is     = client.getInputStream(); //get stream to read from
-        System.out.println(is.read());//(*@\serverBox{4} + \clientBox{3})@*)
-             
+       
+        is     = client.getInputStream();//get stream to read from
+       
+        int var=(int) is.read();
+        System.out.println(var);//(*@\serverBox{4} + \clientBox{3})@*)
+           
+        if(var<=20 &&var>=1)
+        {
+            for (int i =0; i< var;i++)
+            {
+                System.out.println("X");
+            }
+            System.out.println(" ");
+        }
         client.close(); //close connection to client
       }
-      server.close();   //(*@\serverBox{5)}@*)
-    } catch (Throwable t) {
-      t.printStackTrace();
+      server.close();  
+    
+    server.close();
+    } catch (IOException t) {
     }
   }
 }
+
